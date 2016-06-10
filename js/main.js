824a073464b59622f59cca0bf1974cb0b60171dc
Prakse <Prakse>
2016-06-10 23:32:35 +0300

    Background

---
+++ js/main.js
@@ -0,0 +1,13 @@
+$(function () {
+    jQuery(window).scroll(function () {
+        if (jQuery(window).scrollTop() > 1 ) {
+            jQuery(".navbar-default").css("background-color", "rgba(255,255,255,0.9)");
+//            jQuery(".navbar-default").css("border-bottom", "1px solid rgba(170, 26, 35, 0.9)");
+            jQuery(".navbar-default").addClass("animated-nav");
+        } else {
+            jQuery(".navbar-default").css("background-color", "transparent");
+//            jQuery(".navbar-default").css("border-bottom", "0");
+            jQuery(".navbar-default").removeClass("animated-nav");
+        }
+    });
+});
